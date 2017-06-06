080848ac0314bfc96e1713691c158f0b7c5266eb
kqueryful <kqueryful@gmail.com>
2017-06-06 13:03:15 -0700

    More punctuation support, testing
    * support for more punctuation types between kanji & ofurigana
    * added some test cases
    * starting matching for emphasis formatting

--- format.py
+++ format.py
@@ -1,27 +1,31 @@
-
 import re
 
 
-
 def aozora_to_ruby(orig_text):
+    """
+    Convert Aozora Bunko formatted text to ruby tagged HTML
+    :param orig_text: aozora-bunko formatted text
+    :return: ruby tagged HTML
+    """
+
+    text = orig_text
     # paragraphs
-    orig_text = re.sub("\n", "<p>", orig_text)
+    text = re.sub("\n", "<p>", text)
+
+    # emphasis formatting
+    # emphasis_pattern = r""
+    # text = re.sub(emphasis_pattern, "<ruby>\g<text><rt>\g<emphasis></rt></ruby>", text)
 
     # ruby
-    aozora_ruby_pattern = r"(?P<kanji>[㐀-䶵一-鿋豈-頻]+[、]*)《(?P<kana>[^《]+)》"
-    return re.sub(aozora_ruby_pattern, '<ruby>\g<kanji><rt>\g<kana></rt></ruby>', orig_text)
+    aozora_ruby_pattern = r"(?P<kanji>[㐀-䶵一-鿋豈-頻]+)(?P<punc>[、-〿]*)《(?P<kana>[^《]+)》"
+    return re.sub(aozora_ruby_pattern, '<ruby>\g<kanji><rt>\g<kana></rt></ruby>\g<punc>', text)
 
 if __name__ == "__main__":
+    pass
+    # read_file = open("boy.txt", "r")
+    # write_file = open("boy_write.html", 'w')
 
+    # write_file.write('<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" xml:lang="ja" class="vrtl">')
+    # write_file.write('<head><meta charset="UTF-8"/></head>')
 
-    test_line = "　一行がその丘の頂《いただ》きにある尾根《おね》につきさえすれば"
-    print(test_line)
-    print(aozora_to_ruby(test_line))
-
-    read_file = open("boy.txt", "r")
-    write_file = open("boy_write.html", 'w')
-
-    write_file.write('<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" xml:lang="ja" class="vrtl">')
-    write_file.write('<head><meta charset="UTF-8"/></head>')
-
-    write_file.write(aozora_to_ruby(read_file.read()))
+    # write_file.write(aozora_to_ruby(read_file.read()))
---
+++ format_test.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from format import *
+
+class Tests(TestCase):
+    def test_aozora_to_ruby_default(self):
+        # test default expected behavior in converting aozora bunko formatted text to ruby tags
+        test_line = r"　一行がその丘の頂《いただ》きにある尾根《おね》につきさえすれば"
+        expected = "　一行がその丘の<ruby>頂<rt>いただ</rt></ruby>きにある<ruby>尾根<rt>おね</rt></ruby>につきさえすれば"
+        self.assertEqual(expected, aozora_to_ruby(test_line))
+
+    def test_punctuation(self):
+        # want enclosed punctuation to be outside of the ruby tags
+        test_line = "いった瞬間、《しゅんかん》"
+        self.assertEqual("いった<ruby>瞬間<rt>しゅんかん</rt></ruby>、", aozora_to_ruby(test_line))
+
+    def test_punc_double_kanji_words(self):
+        test_line = "から、突然、《とつぜん》恐《おそ》ろしい"
+        self.assertEqual("から、<ruby>突然<rt>とつぜん</rt></ruby>、<ruby>恐<rt>おそ</rt></ruby>ろしい", aozora_to_ruby(test_line))
+
+    def test_em(self):
+        test_line = "はづな《ヽヽヽ》"
+        expected = "<ruby>はづな<rt>ヽヽヽ</rt></ruby>"
+        self.assertEqual(expected, aozora_to_ruby(test_line))
